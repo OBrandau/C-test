73abcf98d50a19d082436e64d9dfdaac87198f9d
Brandau <Oliver.Brandau@wvsi.local>
2018-09-14 12:24:11 +0200

    test

---
+++ dbDemo/dbDemo/Program (F751LDVs in Konflikt stehende Kopie 2016-04-17).cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using System.Data;
+using System.Data.SqlClient;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // verbinden ohne Auswahl um DB zu erzeugen (Server=(localdb)\Projects -> DB ist in SQL Server-Objekt-Explorer sichtbar
+            SqlConnection con = new SqlConnection(@"Server=(localdb)\Projects; Integrated Security=true;"); 
+            String str = "IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'MyDatabase') CREATE DATABASE MyDatabase";
+            SqlCommand cmd = new SqlCommand(str, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+            Console.WriteLine("db created");
+
+
+
+            // Tabelle erzeugen und füllen
+            execNonQuery("IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblPerson') DROP TABLE tblPerson");
+            execNonQuery("CREATE TABLE tblPerson" +
+                "(personId INTEGER PRIMARY KEY," +
+                "name CHAR(50), adress CHAR(255))");
+            Console.WriteLine("table created");
+
+            execNonQuery("INSERT INTO tblPerson(personId, name, adress) " +
+                "VALUES (1, 'Max Mustermann', 'Beispieladresse')");
+            Console.WriteLine("table filled");
+
+            // Tabelle auf Konsole ausgeben
+            con = new SqlConnection("Server=(localdb)\\Projects; Integrated Security=true; Database=MyDatabase;");
+            str = "SELECT * FROM tblPerson";
+            cmd = new SqlCommand(str, con);
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds, "tblPerson");
+
+            foreach (DataRow dr in ds.Tables["tblPerson"].Rows)
+                Console.WriteLine("{0}\t{1}", dr["personId"], dr["name"]);
+
+            Console.Read();
+        }
+
+        static private void execNonQuery(String str)
+        {
+            SqlConnection con = new SqlConnection("Server=(localdb)\\Projects; Integrated Security=true; Database=MyDatabase;");
+            SqlCommand cmd = new SqlCommand(str, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+    }
+}
---
+++ dbDemo/dbDemo/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            SqlConnection con = new SqlConnection(@"Data Source=WAP-SQL2-002; Initial Catalog=DblEic04; Integrated Security=False;User ID=usrdbleic04#02;Password=steffi572;Connect Timeout=15;Encrypt=false;TrustServerCertificate=False");
+            String str = "";
+            SqlCommand cmd = new SqlCommand(str,con);
+            try
+            {
+                con.Open();
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+            Console.WriteLine("db created");
+
+
+            execNonQuery("IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tblMitarbeiter') DROP TABLE tblMitarbeiter");
+            execNonQuery("CREATE TABLE tblMitarbeiter" +
+                "(AbteilungId INTEGER," +
+                "Name CHAR(50))");
+            Console.WriteLine("table created");
+
+            execNonQuery("INSERT INTO tblMitarbeiter(AbteilungId, Name)" +
+                "VALUES (1, 'Peter')");
+            Console.WriteLine("table filled");
+
+            execNonQuery("INSERT INTO tblMitarbeiter(AbteilungId, Name)" +
+               "VALUES (2, 'Hans')");
+            Console.WriteLine("table filled");
+
+            execNonQuery("INSERT INTO tblMitarbeiter(AbteilungId, Name)" +
+               "VALUES (3, 'Horst')");
+            Console.WriteLine("table filled");
+
+
+            str = "SELECT * FROM tblMitarbeiter";
+            cmd = new SqlCommand(str, con);
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds, "tblMitarbeiter");
+            sda.Fill(ds, "tblAbteilung");
+
+
+
+
+            foreach (DataRow dr in ds.Tables["tblMitarbeiter"].Rows)
+            {
+                Console.WriteLine("{0}\t{1}", dr["AbteilungId"], dr["Name"]);
+            }
+
+
+            Console.Read();
+        }
+
+        static private void execNonQuery(String str)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=WAP-SQL2-002; Initial Catalog=DblEic04; Integrated Security=False;User ID=usrdbleic04#02;Password=steffi572;Connect Timeout=15;Encrypt=false;TrustServerCertificate=False");
+            SqlCommand cmd = new SqlCommand(str, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+    }
+}
